5e48fc4eb1db1b194141a44f5d500d63ab30f5b0
Haki-X <hakiahmad.ha@gmail.com>
2025-08-05 12:11:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -62,5 +62,6 @@ if predict :
     prediction = result["prediction"][0]
     prob = result["prob"][0][prediction]
 
-    st.write(f"Your iris species is **{prob: .2f}** **{label}**")
+    st.write(f"Your iris species is **{prob:.0%}{label}**")
+
 
